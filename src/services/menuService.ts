4f06840be5f62c1dba720b49a75a8ba732c3189d
Gabyx708 <matiascoria427@gmail.com>
2024-05-20 16:05:12 -0300

    creacion de las funciones para guardar y recuperar el menu del dia

--- src/services/menuService.ts
+++ src/services/menuService.ts
@@ -1,11 +1,22 @@
-import axios,{AxiosRequestConfig} from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { getUser } from "./userService";
 import axiosInstance from "./axiosInstance";
 
 export const getNextMenuAvailable = async () => {
+  const response = await axiosInstance.get<Menu>("menu/next-available");
+  return response;
+};
 
-     const response = await axiosInstance.get<Menu>('menu/next-available');
-     return response;
+export const saveActualMenu = (menu: Menu): Menu => {
+  sessionStorage.setItem("menu", JSON.stringify(menu));
+  return menu;
+};
 
- };
- 
+export const getActualMenu = (): Menu | null => {
+  const menuStringify: string | null = sessionStorage.getItem("menu");
+  if (menuStringify != null) {
+    return JSON.parse(menuStringify);
+  }
+
+  return null;
+};
